3da99ffcbb75d6266832c934c4240d489d3ea1db
Adorh001 <sougo13.ad@gmail.com>
2020-10-11 19:12:31 -0400

    finished game

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -5,15 +5,40 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     public float speed;
+    public bool right;
     void Start()
     {
-        
+        if(!right)
+        {
+            transform.Rotate(Vector3.back * 180);
+        }
     }
 
 
     void Update()
     {
-        transform.Translate(Vector3.right * speed);
-        transform.Rotate(Vector3.forward);
+        transform.Translate(Vector3.right * speed); //forward is left back is right
+        //transform.Rotate(Vector3.forward);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DirectionalNode dn = other.GetComponent<DirectionalNode>();
+        Debug.Log(dn.current);
+
+        //check direction
+        float rotate = 0;
+        Vector3 dir = Vector3.zero;
+        if (dn.current == "right")
+        {
+            dir = Vector3.back;
+            rotate = 90;
+        }
+        else if (dn.current == "left")
+        {
+            dir = Vector3.forward;
+            rotate = 90;
+        }
+
+        transform.Rotate(dir * rotate);
     }
 }
